14d1c987b341241f56d5f9191655284563a11555
Diego Mussolini <ego159@hotmail.com>
2022-07-28 14:06:43 -0300

    Desafio Mussolini Diego

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import img from "../images/1200px-React.png";
+
+function NavBar(){
+
+    return(
+
+        
+        <ul>
+            <a href=""><img width="100" src={ img }/></a>
+            <a href=""><li>Home</li></a>
+            <a href=""><li>Contact</li></a>
+            <a href=""><li>Carrito</li></a>
+
+        </ul>
+
+
+    )
+
+}
+
+export default NavBar;
